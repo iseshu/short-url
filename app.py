8aaac9ab669e3da199789f8237696cbaf192bc2a
Seshu Sai <105602908+iseshu@users.noreply.github.com>
2022-05-29 17:39:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import string
 import random
 import os
 
-main_data = "SESHUSAI"
+main_data = "SESHUYARRA"
 database = os.environ.get('DATABASE_URL')
 title = os.environ.get('TITLE','Yss Projects')
 logo = os.environ.get('LOGO')
@@ -13,7 +13,6 @@ myclient = pymongo.MongoClient(database)
 mydb = myclient["mydatabase"]
 mycol = mydb["urls"]
 app = Flask(__name__)
-
 def rando():
     res = ''.join(random.choices(string.ascii_lowercase +
                              string.digits, k = 5))
@@ -23,22 +22,32 @@ def rando():
 def main():
     return render_template("main.html",title=title,logo=logo)
 
+def create(url):
+    x= mycol.find_one(main_data)
+    urls = x['urls']
+    ids = x['ids']
+    if url in urls:
+        indx = urls.index(url)
+        id = ids[indx]
+    else:
+        urls.append(url)
+        id = rando()
+        ids.append(id)
+        up = mycol.update_one({"_id":main_data},{"$set":{"urls":urls,"ids":ids}})
+    data = {"status":True,"id":id,"url":url}
+    return data
+
+
 @app.route("/api",methods=['GET'])
 def api():
     url = request.args.get('url')
     if "http" in url:
         x= mycol.find_one(main_data)
-        urls = x['urls']
-        ids = x['ids']
-        if url in urls:
-            indx = urls.index(url)
-            id = ids[indx]
+        if x == None:
+            a = mycol.insert_one({'ids':["create"],"urls":["https://github.com/iseshu/short-url"],"_id":main_data})
+            data = create(url)
         else:
-            urls.append(url)
-            id = rando()
-            ids.append(id)
-            up = mycol.update_one({"_id":main_data},{"$set":{"urls":urls,"ids":ids}})
-        data = {"status":True,"id":id,"url":url}
+            data = create(url)
     else:
         data = {"status":False,"error":"Please Enter A Valid Url"}
     resp = make_response(jsonify(data))
@@ -48,23 +57,15 @@ def api():
 @app.route("/<id>")
 def redir(id):
     x= mycol.find_one(main_data)
-    urls = x['urls']
+    urla = x['urls']
     ids = x['ids']
     if id in ids:
         indx = ids.index(id)
-        url = urls[indx]
+        url = urla[indx]
         return redirect(url)
     else:
         return render_template("error.html",logo=logo)
 
 
 if __name__ == '__main__':
-    x= mycol.find_one(main_data)
-    if x == None:
-        data = {'ids':["create"],"urls":["https://github.com/iseshu/short-url"],"_id":main_data}
-        a = mycol.insert_one(data)
-        print("Data Created")
-        app.run()
-    else:
-        print("Data Already Created")
-        app.run()
+    app.run()
